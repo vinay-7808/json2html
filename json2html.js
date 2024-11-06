8d358b68dd9c6aa42345ca5146e5862eb00079d8
Vinay Srivastava <vinay211113233@gmail.com>
2024-11-06 14:36:58 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,24 +1,25 @@
 // Exporting the json2html function as a default export
 export default function json2html(data) {
+    // Define the required headers
+    const headers = ['Name', 'Age', 'Gender'];
+
     // Start the table and add a custom data attribute
     let html = '<table data-user="vinay211113233@gmail.com">';
 
-    // Generate the table headers dynamically from the keys of the first object
-    if (data.length > 0) {
-        html += '<thead><tr>';
-        Object.keys(data[0]).forEach(key => {
-            html += `<th>${key}</th>`;
-        });
-        html += '</tr></thead>';
-    }
+    // Generate the table headers
+    html += '<thead><tr>';
+    headers.forEach(header => {
+        html += `<th>${header}</th>`;
+    });
+    html += '</tr></thead>';
 
     // Generate the table body
     html += '<tbody>';
     data.forEach(obj => {
         html += '<tr>';
-        Object.keys(data[0]).forEach(key => {
-            // Add each cell, using an empty string if the key doesn't exist in this object
-            html += `<td>${obj[key] || ''}</td>`;
+        headers.forEach(header => {
+            // Add each cell, check if the object has the key, otherwise use an empty string
+            html += `<td>${obj[header] || ''}</td>`;
         });
         html += '</tr>';
     });
